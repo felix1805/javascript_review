61b819ce77c38932de6741294c2bcc7d506d4b0e
felix1805 <felix1805@gmail.com>
2023-08-14 17:25:15 -0400

    added lesson 19 material

--- class16-materials/stop-snacking/js/main.js
+++ class16-materials/stop-snacking/js/main.js
@@ -1,1 +1,9 @@
 //Create a function that grabs the number of snacks from the input and tells you to stop that many times
+document.querySelector('#help').addEventListener('click', snackTime)
+function snackTime() {
+  let input = Number(document.querySelector('input').value)
+  document.querySelector('#stops').innerText = ''
+  for (let index = 0; index < input; index++) {
+  document.querySelector('#stops').innerText +=  ' stop snacking!'
+ }
+}
---
+++ class19-materials/arrays-in-it-to-win-it/js/main.js
@@ -0,0 +1,11 @@
+//Create an array of movies with at least three movies.
+let newArr = ['movieA', 'movieB', 'movieC']
+//Using the array from above, store the first movie in a variable
+let newVar = newArr[0]
+console.log(newVar)
+//Get the length of the original array and store it in a new variable
+let newArrLength = newArr.length
+console.log(newArrLength)
+//Get the last element in that array and store it in a new variable. What if your array was really large and you didn't know the last index? Would your solution still work?
+let lastElement = newArr[newArr.length - 1]
+console.log(lastElement)
