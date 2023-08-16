cdc1e13da1dfd6d6cf263fcf6772dac151e69af0
felix1805 <felix1805@gmail.com>
2023-08-16 17:09:08 -0400

    continued with completing class 20 lessons

--- class20-materials/review-in-paradise-after-paradise/js/main.js
+++ class20-materials/review-in-paradise-after-paradise/js/main.js
@@ -1,1 +1,11 @@
 // Create a function that takes in an array. If the first number, is less than the last number, alert "Hi". If the first number is greater than the last number, alert "Bye". If they are equal, alert "We close in an hour".
+function arrayCompare(array) {
+  if (array[0] < array[array.length - 1]) {
+    alert('hi')
+  } else if (array[0] > array[array.length - 1]) {
+    alert('bye')
+  } else if (array[0] === array[array.length - 1]) {
+    alert('we close in an hour')
+  }
+}
+arrayCompare([1, 2, 3, 4, 1])
--- class20-materials/review-summer-games/js/main.js
+++ class20-materials/review-summer-games/js/main.js
@@ -1,1 +1,11 @@
 //Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+function arrayMultiply(array) {
+  let product = 1
+  // array.forEach(num => product *= num)
+  // OR
+  for (let i = 0; i < array.length; i++) {
+    product *= array[i]
+  }
+  alert(product)
+}
+arrayMultiply([2, 3, 4, 5])
