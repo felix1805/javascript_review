a6f7a4e5e4fd2c8193044ffc2f0302d16cd155e3
felix1805 <felix1805@gmail.com>
2023-09-06 17:00:04 -0400

    working on lesson 31

--- class31-materials/objects-child-class/js/main.js
+++ class31-materials/objects-child-class/js/main.js
@@ -1,2 +1,15 @@
 //Create an a class and extend it - Can be anything you would like it to be! 
 
+class Animal{
+  constructor(name){
+    this.name = name
+  }
+}
+
+class Parrot extends Animal{
+  constructor(name,breed) {
+   super(name)
+    this.breed = breed
+  }
+}
+let tai = new Parrot('tai','dusky conure')
