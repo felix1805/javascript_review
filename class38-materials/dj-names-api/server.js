62b53ca931161ec4132a28cffd41976e7bc1a9e6
felix1805 <felix1805@gmail.com>
2023-09-15 17:26:17 -0400

    first push for dj-names-api, main and server js files and index.html files started and working

---
+++ class38-materials/dj-names-api/server.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const djs = {
+  'hernan cattaneo': {
+    'birthName': 'Hernán Cattáneo',
+    'age': 58,
+    'genre': 'Progressive House',
+    'birthLocation': 'Buenos Aires, Argentina'
+  },
+  'spencer brown': {
+    'birthName': 'Onofrio Spencer Jeffrey Bruno',
+    'age': 29,
+    'genre': 'Progressive House, Techno',
+    'birthLocation': 'Los Gatos, CA, USA'
+  },
+  'nick warren': {
+    'birthName': 'Nicholas John Warren',
+    'age': 54,
+    'genre': 'Electronic dance music, house, breakbeat',
+    'birthLocation': 'Bristol, England'
+  },
+
+}
+
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:djName', (req, res) => {
+  const djsName = req.params.djName.toLowerCase()
+  if (djs[djsName]) {
+    res.json(djs[djsName])
+  } else {
+    res.json(djs)
+    console.log('Please choose one of the listed Djs for a their information to return.')
+  }
+})
+
+app.get('/api/', (req, res) => {
+  res.json(djs)
+})
+
+app.listen(PORT, () => {
+  console.log(`Server Running on Port ${PORT}`)
+})
--- class38-materials/rap-names-api/server.js
+++
@@ -1,4 +0,0 @@
-const express = require('express')
-const app = express()
-
-// 2:08:48 last timestamp
