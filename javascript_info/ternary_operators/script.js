b5407e8b0d45ceb74e617b7f01c36f8b0e3e75d9
felix1805 <felix1805@gmail.com>
2023-12-12 17:19:25 -0500

    several versions of loginFunction added

--- javascript_info/ternary_operators/script.js
+++ javascript_info/ternary_operators/script.js
@@ -1,20 +1,48 @@
-const greetUser = () => {
-  let isValidLogin = true;
+// const greetUser = () => {
+//   let isValidLogin = true;
 
-  while (isValidLogin) {
-      let login = prompt('Please enter your position.');
-      let message =
-          login === 'E' ? 'Hello' :
-          login === 'D' ? 'Greetings' :
-          'No login';
+//   while (isValidLogin) {
+//       let login = prompt('Please enter your position.');
+//       let message =
+//           login === 'E' ? 'Hello' :
+//           login === 'D' ? 'Greetings' :
+//           'No login';
 
-      if (message !== 'No login') {
-          isValidLogin = false;
-      }
+//       if (message !== 'No login') {
+//           isValidLogin = false;
+//       }
 
-      alert(message);
+//       alert(message);
+//   }
+// }
+
+// // Call the function
+// greetUser();
+
+// ================================================
+
+const loginFunction = () => {
+  let login = prompt('please provide your credentials')
+  let message = ''
+  if (login == 'Employee') {
+    message = console.log('hello')
+    console.log(login)
+    
+    
+  } else if (login == 'Director') {
+    message = alert('good day')
+  } else {
+    message = alert('invalid credentials')
+    loginFunction()
   }
 }
+loginFunction()
 
-// Call the function
-greetUser();
+// ================================================
+// let login = prompt('please enter your credentials')
+// let message =
+// login == 'Employee'? 'hello':
+// login == 'Director'? 'good day':
+// 'wrong credentials'
+
+// alert(message)
