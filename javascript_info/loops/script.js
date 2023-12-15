c54c015330551170a7810761e3a2c8edbc08a030
felix1805 <felix1805@gmail.com>
2023-12-15 17:29:37 -0500

    continued working with loops, added prime number checker

--- javascript_info/loops/script.js
+++ javascript_info/loops/script.js
@@ -3,21 +3,21 @@
 // The loop must ask for a number until either the visitor enters a number greater than 100 or cancels the input/enters an empty line.
 
 // Here we can assume that the visitor only inputs numbers. There’s no need to implement a special handling for a non-numeric input in this task.
-let num = 0
+// let num = 0
 
-while (num <= 100) {
-  num = +prompt('please enter a number greater than 100')
-  if (num > 100) {
-    alert('great')
-  } else if (num < 100) {
-    +prompt(`Please enter a value greater than 100, ${num} is not enough.`)
-    if (num < 100) {
-      +prompt(`Please enter a value greater than 100, ${num} is still not enough.`)
-    } else if (num > 100) {
-      alert('there you go!')
-    }
-  }
-}
+// while (num <= 100) {
+//   num = +prompt('please enter a number greater than 100')
+//   if (num > 100) {
+//     alert('great')
+//   } else if (num < 100) {
+//     +prompt(`Please enter a value greater than 100, ${num} is not enough.`)
+//     if (num < 100) {
+//       +prompt(`Please enter a value greater than 100, ${num} is still not enough.`)
+//     } else if (num > 100) {
+//       alert('there you go!')
+//     }
+//   }
+// }
 
 // let num = 0;
 // let firstTime = true;
@@ -44,5 +44,109 @@ while (num <= 100) {
 //     alert('Great!');
 //   } else {
 //     alert(`Please enter a value greater than 100, ${num3} is not enough.`);
+//   }
+// }
+
+
+// let i = 0
+
+// while (i<3) {
+//   alert(`number ${i}!`)
+//   i++
+// }
+
+// let num = prompt('please insert a number')
+// let i = 1
+// while (i<=num) {
+//   let result = 1
+//   let num2 = result*i
+//   if (isPrime(num2)) {
+
+//     console.log(num2)
+//   }
+//   i++
+// }
+
+// let n;
+// n = prompt("Enter a number greater than 1");
+// if (n <= 1)
+// alert("Invalid input");
+// else if (n == 2)
+// alert("The number is a prime number");
+// else {
+// for (i = 2; i < n; i++) {
+// if(n%i==0){
+// alert("The number is not a prime number");
+// break;
+// }
+// }
+// if(i == n )
+// alert("The number is a prime number");
+// }
+
+// let num = prompt('Please insert a number');
+// let i = 2; // Start checking from 2, as 1 is not a prime number
+// console.log(num)
+
+// while (i <= num) {
+//   let isPrime = true;
+//   for (let j = 2; j <= i; j++) {
+//     if (i % j === 0) {
+//       console.log('value of i= ' + i)
+//       console.log('value of j= ' + j)
+//       console.log('value of i/j= ' + i/j)
+//       isPrime = false;
+//       break;
+//     }
+//   }
+
+//   // Output i if it is a prime number
+//   if (isPrime) {
+//     console.log(i + " is a prime number.");
+//   }
+
+//   i++;
+// }
+
+
+let n = prompt("Please input a number");
+
+for (let i = 2; i <= n; i++) {
+    let isPrime = true; // Flag to track whether i is prime
+
+    for (let j = 2; j < i; j++) { //removed <= Math.sqrt(i) for testing
+        if (i % j === 0) {
+            isPrime = false; // Set the flag to false if a divisor is found
+            break;
+        }
+    }
+
+    if (isPrime) {
+        alert(i);
+        console.log(i)
+    }
+}
+
+
+
+// =======================
+
+
+// let num = prompt("Tell me the number");
+
+// for (let i = 2; i <= num; i++) {
+//   let count = 0;
+
+//   // Check for divisibility up to the square root of i
+//   for (let j = 2; j <= Math.sqrt(i); j++) {
+//     if (i % j === 0) {
+//       ++count;
+//       break; // Exit the loop early if a divisor is found
+//     }
+//   }
+
+//   // Output i if it is a prime number
+//   if (count === 0) {
+//     alert(i);
 //   }
 // }
