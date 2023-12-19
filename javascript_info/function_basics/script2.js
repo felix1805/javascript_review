2669f34ba3c52b9b6a6774d4767342b0b321aa90
felix1805 <felix1805@gmail.com>
2023-12-19 17:01:55 -0500

    completed function basics, modified exponent function

--- javascript_info/function_basics/script2.js
+++ javascript_info/function_basics/script2.js
@@ -1,7 +1,39 @@
-function pow(a,b) {
-  let var1 = a*b
-  document.querySelector('h1').innerText = `${var1}`
-  return
+
+
+let base = +prompt('Enter value for x', 'x');
+let exponent = +prompt('Enter value for n', 'n');
+
+
+function pow(base, exponent) {
+  let result = base; // set initial value (3 for example)
+  let calculationString = `${base}`; // initialize the string with the base
+
+  for (let i = 1; i < exponent; i++) {
+    result = result * base; //returns intial multiplication, 3*3, loop runs twice (var2 -1), as ^3 requires 2 multiplication operations, thus returns 27
+
+    calculationString += ` * ${base}`; // append each multiplication step to the string
+  }
+
+  document.querySelector('h1').innerText = `${calculationString} = ${result}`;
 }
 
-pow(20,2)
+pow(base, exponent);
+
+/////////////////////////////////////
+
+let n = +prompt('please enter a value to check for primes leading up to value')
+
+for (let i = 2; i <= n; i++) {
+  let isPrime = true
+  for (let j = 2; j < i; j++) {
+    if (i % j === 0) {
+      isPrime = false
+      console.log(i + " is not a prime number" + j + ' = value of j')
+      break
+    }
+  }
+  if (isPrime) {
+    console.log(i + ' is a prime number')
+  }
+}
+
