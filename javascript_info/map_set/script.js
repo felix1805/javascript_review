2c5a9e496c74c912f14427059efa533fd1875f3e
felix1805 <felix1805@gmail.com>
2024-01-22 17:11:14 -0500

    added destructuring lesson

--- javascript_info/map_set/script.js
+++ javascript_info/map_set/script.js
@@ -1,83 +1,102 @@
-let vehicleMap = new Map([
-  ['BMW', 325],
-  ['Audi', 'S4'],
-  ['Mercedes', 'C300']
-])
-vehicleMap.forEach((val, key, map) => {
-  console.log(`${key}: ${val}`)
-})
-///////////////////////
+// let vehicleMap = new Map([
+//   ['BMW', 325],
+//   ['Audi', 'S4'],
+//   ['Mercedes', 'C300']
+// ])
+// vehicleMap.forEach((val, key, map) => {
+//   console.log(`${key}: ${val}`)
+// })
+// ///////////////////////
 
-let users = new Set()
+// let users = new Set()
 
-let bill = { name: 'Bill', age: 33, props: [12, 33, 33, 44] }
-let jim = { name: 'Jim' }
-let john = { name: 'John' }
+// let bill = { name: 'Bill', age: 33, props: [12, 33, 33, 44] }
+// let jim = { name: 'Jim' }
+// let john = { name: 'John' }
 
-users.add(bill)
-users.add(bill)
-users.add(john)
-users.add(john)
-users.add(jim)
-users.add(john)
-users.add(john)
+// users.add(bill)
+// users.add(bill)
+// users.add(john)
+// users.add(john)
+// users.add(jim)
+// users.add(john)
+// users.add(john)
 
-for (let user of users) {
-  console.log(user.name)
+// for (let user of users) {
+//   console.log(user.name)
+// }
+// console.log(users.size)
+// /////////////////////////
+
+// let cars = new Map()
+
+// cars.set('1', 'BMW')
+// cars.set('2', 'Mercedes')
+// cars.set('3', 'VW')
+
+// console.log(cars)
+
+// cars.forEach((value, key, map) => {
+//   console.log(`${key}: ${value} is the car selected`)
+// })
+
+// ////////////////////////
+
+// function unique(arr) {
+
+//   return Array.from(new Set(arr));
+
+// }
+
+
+// let values = ["Butter", "Sauce", "Butter", "Chicken",
+//   "Butter", "Beans", "Sauce", "Sauce", ":-O"
+// ];
+
+
+
+// console.log(unique(values));
+
+
+// ///////////////////////
+
+
+// function sumSalaries(salaries) {
+//   let sum = 0
+//   for (let salary of Object.values(salaries)) {
+//     sum += salary
+//   }
+//   return sum
+// }
+
+// let salaries = {
+//   'john': 300,
+//   'bill': 400,
+//   'mary': 400
+// }
+// console.log(sumSalaries(salaries))
+
+// function countObj(salaries) {
+//   return Object.keys(salaries).length
+// }
+
+// console.log(countObj(salaries))
+
+////////////////////////////////
+
+let user = {
+  name: 'Felix',
+  surName: 'Petzsche'
 }
-console.log(users.size)
-/////////////////////////
 
-let cars = new Map()
-
-cars.set('1', 'BMW')
-cars.set('2', 'Mercedes')
-cars.set('3', 'VW')
-
-console.log(cars)
-
-cars.forEach((value, key, map) => {
-  console.log(`${key}: ${value} is the car selected`)
-})
-
-////////////////////////
-
-function unique(arr) {
-
-  return Array.from(new Set(arr));
-
+let {name, surName} = user
+function ageMod(inputVal) {
+user.age = 30
+let newAge = user.age + inputVal
+user.age = newAge
+return newAge
 }
+ageMod(43)
 
 
-let values = ["Butter", "Sauce", "Butter", "Chicken",
-  "Butter", "Beans", "Sauce", "Sauce", ":-O"
-];
-
-
-
-console.log(unique(values));
-
-
-///////////////////////
-
-
-function sumSalaries(salaries) {
-  let sum = 0
-  for (let salary of Object.values(salaries)) {
-    sum += salary
-  }
-  return sum
-}
-
-let salaries = {
-  'john': 300,
-  'bill': 400,
-  'mary': 400
-}
-console.log(sumSalaries(salaries))
-
-function countObj(salaries) {
-  return Object.keys(salaries).length
-}
-
-console.log(countObj(salaries))
+console.log(user)
