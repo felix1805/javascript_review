960c23c96e53c522711ed7a9b717cc21ce3e2246
felix1805 <felix1805@gmail.com>
2024-01-29 17:07:37 -0500

    added functions to adv_functions

--- javascript_info/advanced_functions/script.js
+++ javascript_info/advanced_functions/script.js
@@ -1,33 +1,66 @@
-// iterative method to solve a power function
+// // iterative method to solve a power function
 
-function toPower(x, n) {
-  let result = 1
-  for (let i = 0; i < n; i++) {
-    result *= x
+// function toPower(x, n) {
+//   let result = 1
+//   for (let i = 0; i < n; i++) {
+//     result *= x
+
+//   }
+//   return result
+// }
+
+// console.log(toPower(4, 2))
+
+// // recursive method = simplyfy function into multiple easier steps
+// // When a function solves a task, in the process it can call many other functions. A partial case of this is when a function calls itself. That’s called recursion.
+
+// function recursionPow(x, n) {
+//   if (n == 1) {
+//     return x
+//   } else {
+//     return x * recursionPow(x, n - 1)
+//   }
+// }
+// console.log(recursionPow(3, 4))
+
+// // same function using conditional operator
+
+// function recursionCond(x, n) {
+//   return (n == 1) ? x : x * recursionCond(x, n - 1)
+// }
+// console.log(recursionCond(3,4))
+
+// ////////////////////////////////
+
+// Recursion for loop to add up numbers to n with i<=n
+function recurAdd(num) {
+  let result = 0
+  for (let i = 1; i <= num; i++) {
+    result += i
 
   }
   return result
 }
 
-console.log(toPower(4, 2))
+console.log(recurAdd(4))
 
-// recursive method = simplyfy function into multiple easier steps
-// When a function solves a task, in the process it can call many other functions. A partial case of this is when a function calls itself. That’s called recursion.
+// Recursion for loop to multiply numbers up to n with i<=n
 
-function recursionPow(x, n) {
-  if (n == 1) {
-    return x
-  } else {
-    return x * recursionPow(x, n - 1)
+function recurMulti(num) {
+  let result = 1
+  for (let i = 1; i <= num; i++) {
+    result *= i
+
   }
+  return result
 }
-console.log(recursionPow(3, 4))
+console.log(recurMulti(5))
 
-// same function using conditional operator
+/// using terenary operator
 
-function recursionCond(x, n) {
-  return (n == 1) ? x : x * recursionCond(x, n - 1)
+function terenaryMulti(n) {
+  return n ? n * terenaryMulti(n - 1) : 1
 }
-console.log(recursionCond(3,4))
 
-////////////////////////////////
+console.log(terenaryMulti(5))
+
